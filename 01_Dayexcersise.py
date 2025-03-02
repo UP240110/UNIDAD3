3088159e2967241067caf7c58a650a4a54246469
UP240110 <aripesa021806@gmail.com>
2025-03-01 23:36:47 -0600

    Add files via upload

---
+++ 01_Dayexcersise.py
@@ -0,0 +1,25 @@
+print(4 + 3)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 % 4)
+print(3 // 4)
+
+Name = 'Jonathan Ariel'
+Family = 'Peñaloza Santillan'
+Country = 'Mexico'
+Python = 'Python'
+
+print('Nombre:', Name)
+print('Familia:', Family)
+print('Pais:', Country)
+print('I am enjoying 30 days of python')
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 + 4j))
+print(type('Jonathan Ariel'))
+print(type([Name, Python, Country]))
+print(type({'Name': Name, 'Python': Python, 'Country': Country}))
